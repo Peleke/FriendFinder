f272b5044e00022424d83f8fe268ab51f4e6c8ac
Jake DeMonaco <jakedemonaco@MacBook-Pro.home>
2017-08-22 17:23:25 -0400

    almost finished survey button not working and there is a problem with the div container html and body tags

---
+++ app/data/friends.js
@@ -0,0 +1,73 @@
+var friends = [
+
+	{
+		name: "Jay-Z",
+		photo: "https://images-na.ssl-images-amazon.com/images/M/MV5BNDcwOTc0NjI0OV5BMl5BanBnXkFtZTcwNjE1MDc0OQ@@._V1_UY317_CR6,0,214,317_AL_.jpg",
+		scores: [
+			"5",
+			"4",
+			"3",
+			"2",
+			"1",
+			"1",
+			"2",
+			"3",
+			"4",
+			"5"
+		]
+	},
+
+	{
+		name: "Kanye West",
+		photo: "http://dazedimg.dazedgroup.netdna-cdn.com/1012/azure/dazed-prod/1150/4/1154856.jpg",
+		scores: [
+			"1",
+			"2",
+			"3",
+			"4",
+			"5",
+			"5",
+			"4",
+			"3",
+			"2",
+			"1"
+		]
+	},
+
+	{
+		name: "Barack Obama",
+		photo: "http://instinctmagazine.com/sites/instinctmagazine.com/files/images/blog_posts/Nigel%20Campbell/2014/06/10/obama%20russia.jpg",
+		scores: [
+			"1",
+			"5",
+			"1",
+			"5",
+			"1",
+			"5",
+			"1",
+			"5",
+			"1",
+			"5"
+		]
+	},
+
+	{
+		name: "Donald Trump",
+		photo: "http://static4.businessinsider.com/image/56c640526e97c625048b822a-480/donald-trump.jpg",
+		scores: [
+			"5",
+			"1",
+			"5",
+			"1",
+			"5",
+			"1",
+			"5",
+			"1",
+			"5",
+			"1"
+		]
+	}
+
+];
+
+module.exports = friends;
